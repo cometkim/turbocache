a1f0e9258130b88d393f1b20e582dbf161a718ef
Hyeseong Kim <hey@hyeseong.kim>
2023-02-01 22:09:27 +0900

    migrate to module worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,66 +1,63 @@
 /// <reference types="@cloudflare/workers-types" />
 
-declare var STORAGE: KVNamespace;
+type Context = {
+  /**
+   * @see https://developers.cloudflare.com/workers/wrangler/commands/#kvnamespace
+   */
+  STORAGE: KVNamespace;
+
+  /**
+   * @see https://developers.cloudflare.com/workers/cli-wrangler/commands#secret
+   */
+  SECRET_KEY: string;
+}
+
+const handler: ExportedHandler<Context> = {
+  async fetch(request, env, _ctx) {
+    const url = new URL(request.url);
+
+    const storageRoutePattern = /^\/v8\/artifacts\/(?<hash>\w+)$/;
+    const match = url.pathname.match(storageRoutePattern);
+
+    if (!match) {
+      return new Response(null, { status: 404 });
+    }
+
+    if (request.headers.get('Authorization') !== `Bearer ${env.SECRET_KEY}`) {
+      return new Response('Request not permitted', { status: 401 });
+    }
+
+    const { hash } = match.groups!;
+
+    switch (request.method) {
+      case 'HEAD': {
+        return headArtifact(env.STORAGE, hash);
+      }
+
+      case 'GET': {
+        return getArtifact(env.STORAGE, hash);
+      }
+
+      case 'PUT': {
+        if (!request.body) {
+          return new Response('Request body cannot be empty', { status: 400 });
+        }
+        return putArtifact(env.STORAGE, hash, request.body);
+      }
+
+      default: {
+        return new Response('Method not allowed', { status: 405 });
+      }
+    }
+  },
+};
+
+export default handler;
+
 const key = (hash: string) => `artifact:${hash}`;
 
-// See https://developers.cloudflare.com/workers/cli-wrangler/commands#secret
-declare var SECRET_KEY: string;
-
-addEventListener('fetch', event => {
-  const { request } = event;
-  const url = new URL(request.url);
-
-  const storageRoutePattern = /^\/v8\/artifacts\/(?<hash>\w+)$/;
-  const match = url.pathname.match(storageRoutePattern);
-
-  if (!match) {
-    return void event.respondWith(
-      new Response(null, { status: 404 }),
-    );
-  }
-
-  if (request.headers.get('Authorization') !== `Bearer ${SECRET_KEY}`) {
-    return void event.respondWith(
-      new Response('Request not permitted', { status: 401 }),
-    );
-  }
-
-  const { hash } = match.groups!;
-
-  switch (request.method) {
-    case 'HEAD': {
-      return void event.respondWith(
-        headArtifact(hash),
-      );
-    }
-
-    case 'GET': {
-      return void event.respondWith(
-        getArtifact(hash),
-      );
-    }
-
-    case 'PUT': {
-      if (!request.body) {
-        return void event.respondWith(
-          new Response('Request body cannot be empty', { status: 400 }),
-        );
-      }
-      return void event.respondWith(
-        putArtifact(hash, request.body),
-      );
-    }
-
-    default: {
-      return void event.respondWith(
-        new Response('Method not allowed', { status: 405 }),
-      );
-    }
-  }
-});
-
-async function headArtifact(hash: string): Promise<Response> {
-  const artifact = await STORAGE.get(key(hash), 'stream');
+async function headArtifact(namespace: KVNamespace, hash: string): Promise<Response> {
+  const artifact = await namespace.get(key(hash), 'stream');
   if (artifact) {
     return new Response(null);
   } else {
@@ -68,8 +65,8 @@ async function headArtifact(hash: string): Promise<Response> {
   }
 }
 
-async function getArtifact(hash: string): Promise<Response> {
-  const artifact = await STORAGE.get(key(hash), 'stream');
+async function getArtifact(namespace: KVNamespace, hash: string): Promise<Response> {
+  const artifact = await namespace.get(key(hash), 'stream');
   if (artifact) {
     return new Response(artifact);
   } else {
@@ -77,9 +74,9 @@ async function getArtifact(hash: string): Promise<Response> {
   }
 }
 
-async function putArtifact(hash: string, artifact: ReadableStream): Promise<Response> {
+async function putArtifact(namespace: KVNamespace, hash: string, artifact: ReadableStream): Promise<Response> {
   try {
-    await STORAGE.put(key(hash), artifact);
+    await namespace.put(key(hash), artifact);
     return new Response(null, { status: 204 });
   } catch (e) {
     console.error(e);
